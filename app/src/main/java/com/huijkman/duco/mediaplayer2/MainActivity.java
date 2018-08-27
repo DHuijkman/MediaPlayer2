789ba9b39b13e8b9c06b53972934bd1f9fd88d04
ducohuijkman <d.b.huijkman@gmail.com>
2018-08-28 00:06:28 +0200

    added some comments nothing special

--- app/src/main/java/com/huijkman/duco/mediaplayer2/MainActivity.java
+++ app/src/main/java/com/huijkman/duco/mediaplayer2/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //pop up a permission request if needed if not continue running the the rest of the app
         if(ContextCompat.checkSelfPermission(MainActivity.this,
                 Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
             if(ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
@@ -42,22 +43,29 @@ public class MainActivity extends AppCompatActivity {
             runApp();
         }
     }
-
+    //after permission to read storage has been granted start setting up everything
     public void runApp(){
         lv = (ListView) findViewById(R.id.lvPlayList);
 
+        //call the findsongs method to get a list of all mp3 files on your phone
         final ArrayList<File> mySongs = findSongs(Environment.getExternalStorageDirectory());
+        //create a string array (to keep the names of the files in) with the same size as mySongs
         items = new String[mySongs.size()];
+        //fill items and at the same time remove the '.mp3' part of the string to tidy it up
         for(int i = 0; i <mySongs.size(); i++){
             //toast(mySongs.get(i).getName());
             items[i] = mySongs.get(i).getName().replace(".mp3","");
         }
 
+        //set new adapter for the listview
         ArrayAdapter<String> adp = new ArrayAdapter<>(getApplicationContext(),R.layout.song_layout,R.id.textView,items);
         lv.setAdapter(adp);
+        //set listener for the listview so that when you click on a song something (new activitiy) actualy happens
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //start new activity (the song control screen where you can:
+                // fastforward/backward,play/pause,control seekbar,skip to previous or next song
                 startActivity(new Intent(getApplicationContext(),
                         Player.class).putExtra("pos",position).putExtra("songlist",mySongs));
             }
@@ -65,24 +73,35 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public ArrayList<File> findSongs(File root){
+        //make new array which will hold return value(the songs that have been found)
         ArrayList<File> al = new ArrayList<File>();
+        //make array with type File and fill it with the files from the directory that has been giving
         File[] files = root.listFiles();
+        //iterate through files and recursively look through all(including subdirectories) files
         for(File singleFile : files){
+            //check subdirectory as long as its not a hidden file
             if(singleFile.isDirectory() && !singleFile.isHidden()){
                 al.addAll(findSongs(singleFile));
             }else{
+                //if the name of current file ends with .mp3 it gets added note that i also made it
+                //so that files starting with a '.' got rejected because of certain files on my phone
+                //its comletely optional you for example add another requirement saying
+                // 'singleFile.endsWith(".wav")' to let diffrent kind of extensions also get added
                 if(singleFile.getName().endsWith(".mp3") && !singleFile.getName().startsWith(".")){
                     al.add(singleFile);
                 }
             }
         }
+        //return the list of music files
         return al;
     }
 
     public void toast(String text){
+        //some test shit i cba to remove
         Toast.makeText(getApplicationContext(),text,Toast.LENGTH_SHORT).show();
     }
 
+    //this so the method runApp actually gets called after giving permissions to read storage
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         switch (requestCode){
--- app/src/main/java/com/huijkman/duco/mediaplayer2/Player.java
+++ app/src/main/java/com/huijkman/duco/mediaplayer2/Player.java
@@ -13,7 +13,7 @@ import java.io.File;
 import java.util.ArrayList;
 
 public class Player extends AppCompatActivity implements View.OnClickListener{
-
+    //im not gonna explain these -.-
     static MediaPlayer mp;
     ArrayList<File> mySongs;
     Uri u;
@@ -24,17 +24,20 @@ public class Player extends AppCompatActivity implements View.OnClickListener{
 
     int position;
 
+    //init everything when the activity is created
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
 
+        //the buttons connected to their imageviews
         btPlay = (ImageView) findViewById(R.id.btPlay);
         btFF = (ImageView) findViewById(R.id.btFF);
         btFB = (ImageView) findViewById(R.id.btFB);
         btNxt = (ImageView) findViewById(R.id.btNxt);
         btPv = (ImageView) findViewById(R.id.btPv);
 
+        //the seekbar and its sepperate thread to keep an eye on the progress and display it
         sb = (SeekBar) findViewById(R.id.seekBar);
         updateSeekBar = new Thread(){
             @Override
@@ -53,30 +56,36 @@ public class Player extends AppCompatActivity implements View.OnClickListener{
                 }
             }
         };
-
+        //the click listeners to control the music
         btPlay.setOnClickListener(this);
         btFF.setOnClickListener(this);
         btFB.setOnClickListener(this);
         btNxt.setOnClickListener(this);
         btPv.setOnClickListener(this);
 
+        //basicaly the array of songs is brought here from the music list to the controller
+        //simply speaking
         Intent i = getIntent();
         Bundle b = i.getExtras();
         mySongs = (ArrayList) b.getParcelableArrayList("songlist");
         position = b.getInt("pos",0);
 
+        //if the mediaplayer equals null the stop the music and release its resources this is used
+        //to stop a song when you select a new one
         if(mp != null){
             mp.stop();
             mp.release();
         }
-
+        //this part till mp.start basicaly starts the song after selecting it if you dont get it
+        // google Uri and MediaPlayer.create
         u = Uri.parse( mySongs.get(position).toString() );
         mp = MediaPlayer.create(getApplicationContext(),u);
         mp.start();
+        //start the thread for the seekbar
         updateSeekBar.start();
-
+        //set the max value (how long the song is in milliseconds) for the seekbar
         sb.setMax(mp.getDuration());
-
+        //set the listeners for the seekbar
         sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
@@ -90,16 +99,19 @@ public class Player extends AppCompatActivity implements View.OnClickListener{
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
+                //this allows you to skip to a certain point in the song(again in milliseconds)
                 mp.seekTo(seekBar.getProgress());
             }
         });
     }
 
+    //onclick method for the control buttons
     @Override
     public void onClick(View v) {
         int id = v.getId();
         switch (id){
             case R.id.btPlay:
+                //if music is playing pause the music and change the image vise versa for the else part
                 if(mp.isPlaying()){
                     btPlay.setImageResource(android.R.drawable.ic_media_play);
                     mp.pause();
@@ -109,29 +121,41 @@ public class Player extends AppCompatActivity implements View.OnClickListener{
                 }
                 break;
             case R.id.btFF:
+                //skip 5 seconds ahead in the song
                 mp.seekTo(mp.getCurrentPosition()+5000);
                 break;
             case R.id.btFB:
+                //go back 5 seconds
                 mp.seekTo(mp.getCurrentPosition()-5000);
                 break;
             case R.id.btNxt:
+                //stop current song and release resources
                 mp.stop();
                 mp.release();
+                //move up the position by one
                 position = (position+1)%mySongs.size();
+                //start new song
                 u = Uri.parse( mySongs.get(position).toString() );
                 mp = MediaPlayer.create(getApplicationContext(),u);
                 mp.start();
+                //set new max value for the seekbar
                 sb.setMax(mp.getDuration());
+                //update play button
                 btPlay.setImageResource(android.R.drawable.ic_media_pause);
                 break;
             case R.id.btPv:
+                //stop current song and release resources
                 mp.stop();
                 mp.release();
+                //go down position by one
                 position = (position-1<0)? mySongs.size() -1: position -1;
+                //start new song
                 u = Uri.parse( mySongs.get(position).toString() );
                 mp = MediaPlayer.create(getApplicationContext(),u);
                 mp.start();
+                //set new max value for seekbar
                 sb.setMax(mp.getDuration());
+                //update play button
                 btPlay.setImageResource(android.R.drawable.ic_media_pause);
                 break;
         }
