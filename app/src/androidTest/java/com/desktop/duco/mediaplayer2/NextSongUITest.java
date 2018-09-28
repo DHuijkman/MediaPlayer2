9e19384277c9d54aa8223e311dd6624bb9adf5ca
Duco Huijkman <d.b.huijkman@gmail.com>
2018-09-28 12:16:58 +0200

    branch test

--- app/src/androidTest/java/com/desktop/duco/mediaplayer2/NextSongUITest.java
+++ app/src/androidTest/java/com/desktop/duco/mediaplayer2/NextSongUITest.java
@@ -1,111 +1,111 @@
-package com.desktop.duco.mediaplayer2;
-
-
-import android.Manifest;
-import android.support.test.espresso.ViewInteraction;
-import android.support.test.filters.LargeTest;
-import android.support.test.rule.ActivityTestRule;
-import android.support.test.rule.GrantPermissionRule;
-import android.support.test.runner.AndroidJUnit4;
-
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
-
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.swipeLeft;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withParent;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.allOf;
-
-@LargeTest
-@RunWith(AndroidJUnit4.class)
-public class NextSongUITest {
-
-    @Rule
-    public GrantPermissionRule writePermission = GrantPermissionRule.grant(Manifest.permission.WRITE_EXTERNAL_STORAGE);
-    //asdkjkbjbjk
-    @Rule
-    public GrantPermissionRule readPermission = GrantPermissionRule.grant(Manifest.permission.READ_EXTERNAL_STORAGE);
-
-    @Rule
-    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
-
-
-
-    @Test
-    public void nextSongUITest() {
-        ViewInteraction appCompatTextView = onView(
-                allOf(withId(R.id.title), withText("Can't Take My Eyes Off You - Frankie Valli (Lyrics)"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.recycler_view),
-                                        1),
-                                1),
-                        isDisplayed()));
-        appCompatTextView.perform(click());
-
-        ViewInteraction viewPager = onView(
-                allOf(withId(R.id.pager),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                0),
-                        isDisplayed()));
-        viewPager.perform(swipeLeft());
-
-        ViewInteraction appCompatImageView = onView(
-                allOf(withId(R.id.btNxt),
-                        childAtPosition(
-                                withParent(withId(R.id.pager)),
-                                5),
-                        isDisplayed()));
-        appCompatImageView.perform(click());
-
-        ViewInteraction appCompatImageView2 = onView(
-                allOf(withId(R.id.btNxt),
-                        childAtPosition(
-                                withParent(withId(R.id.pager)),
-                                5),
-                        isDisplayed()));
-        appCompatImageView2.perform(click());
-
-        ViewInteraction appCompatImageView3 = onView(
-                allOf(withId(R.id.btPlay),
-                        childAtPosition(
-                                withParent(withId(R.id.pager)),
-                                1),
-                        isDisplayed()));
-        appCompatImageView3.perform(click());
-    }
-
-    private static Matcher<View> childAtPosition(
-            final Matcher<View> parentMatcher, final int position) {
-
-        return new TypeSafeMatcher<View>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
-    }
-}
+//package com.desktop.duco.mediaplayer2;
+//
+//
+//import android.Manifest;
+//import android.support.test.espresso.ViewInteraction;
+//import android.support.test.filters.LargeTest;
+//import android.support.test.rule.ActivityTestRule;
+//import android.support.test.rule.GrantPermissionRule;
+//import android.support.test.runner.AndroidJUnit4;
+//
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.view.ViewParent;
+//
+//import org.hamcrest.Description;
+//import org.hamcrest.Matcher;
+//import org.hamcrest.TypeSafeMatcher;
+//import org.junit.Rule;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//
+//import static android.support.test.espresso.Espresso.onView;
+//import static android.support.test.espresso.action.ViewActions.click;
+//import static android.support.test.espresso.action.ViewActions.swipeLeft;
+//import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+//import static android.support.test.espresso.matcher.ViewMatchers.withId;
+//import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+//import static android.support.test.espresso.matcher.ViewMatchers.withText;
+//import static org.hamcrest.Matchers.allOf;
+//
+//@LargeTest
+//@RunWith(AndroidJUnit4.class)
+//public class NextSongUITest {
+//
+//    @Rule
+//    public GrantPermissionRule writePermission = GrantPermissionRule.grant(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+//    //asdkjkbjbjk
+//    @Rule
+//    public GrantPermissionRule readPermission = GrantPermissionRule.grant(Manifest.permission.READ_EXTERNAL_STORAGE);
+//
+//    @Rule
+//    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+//
+//
+//
+//    @Test
+//    public void nextSongUITest() {
+//        ViewInteraction appCompatTextView = onView(
+//                allOf(withId(R.id.title), withText("Can't Take My Eyes Off You - Frankie Valli (Lyrics)"),
+//                        childAtPosition(
+//                                childAtPosition(
+//                                        withId(R.id.recycler_view),
+//                                        1),
+//                                1),
+//                        isDisplayed()));
+//        appCompatTextView.perform(click());
+//
+//        ViewInteraction viewPager = onView(
+//                allOf(withId(R.id.pager),
+//                        childAtPosition(
+//                                childAtPosition(
+//                                        withId(android.R.id.content),
+//                                        0),
+//                                0),
+//                        isDisplayed()));
+//        viewPager.perform(swipeLeft());
+//
+//        ViewInteraction appCompatImageView = onView(
+//                allOf(withId(R.id.btNxt),
+//                        childAtPosition(
+//                                withParent(withId(R.id.pager)),
+//                                5),
+//                        isDisplayed()));
+//        appCompatImageView.perform(click());
+//
+//        ViewInteraction appCompatImageView2 = onView(
+//                allOf(withId(R.id.btNxt),
+//                        childAtPosition(
+//                                withParent(withId(R.id.pager)),
+//                                5),
+//                        isDisplayed()));
+//        appCompatImageView2.perform(click());
+//
+//        ViewInteraction appCompatImageView3 = onView(
+//                allOf(withId(R.id.btPlay),
+//                        childAtPosition(
+//                                withParent(withId(R.id.pager)),
+//                                1),
+//                        isDisplayed()));
+//        appCompatImageView3.perform(click());
+//    }
+//
+//    private static Matcher<View> childAtPosition(
+//            final Matcher<View> parentMatcher, final int position) {
+//
+//        return new TypeSafeMatcher<View>() {
+//            @Override
+//            public void describeTo(Description description) {
+//                description.appendText("Child at position " + position + " in parent ");
+//                parentMatcher.describeTo(description);
+//            }
+//
+//            @Override
+//            public boolean matchesSafely(View view) {
+//                ViewParent parent = view.getParent();
+//                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+//                        && view.equals(((ViewGroup) parent).getChildAt(position));
+//            }
+//        };
+//    }
+//}
